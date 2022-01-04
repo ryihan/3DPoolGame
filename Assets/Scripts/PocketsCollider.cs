9c333d541bcc2e6b07289d7fd7fa42d0b7b6aa42
raihan <54474184+ryihan@users.noreply.github.com>
2022-01-05 00:42:32 +0600

    Create PocketsCollider.cs

---
+++ Assets/Scripts/PocketsCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using ThreeDPool.Controllers;
+
+namespace ThreeDPool
+{
+    class PocketsCollider : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider collider)
+        {
+            CueBallController cueBall = collider.gameObject.GetComponent<CueBallController>();
+            if (cueBall != null)
+                cueBall.BallPocketed();
+        }
+    }
+}
