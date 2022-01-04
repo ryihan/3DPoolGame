e8548b61fa97215f54043bb00a18a8c7180b202f
raihan <54474184+ryihan@users.noreply.github.com>
2022-01-05 00:45:30 +0600

    Create PlayerUIController.cs

---
+++ Assets/Scripts/UIControllers/PlayerUIController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+namespace ThreeDPool.UIControllers
+{
+    public class PlayerUIController : MonoBehaviour
+    {
+        public Text NameNScore;
+        public Image TurnMarker;
+    }
+}
