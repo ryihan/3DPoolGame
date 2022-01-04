1a81fa3e5f56380fc560c44284a99bb60905371c
raihan <54474184+ryihan@users.noreply.github.com>
2022-01-04 22:09:13 +0600

    Create CueActionEvent.cs

---
+++ Assets/Scripts/EventHandlers/CueActionEvent.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ThreeDPool.EventHandlers
+{
+    // this event system will be helpful for UI implementation
+    public struct CueActionEvent : IGameEvent
+    {
+        public float ForceGathered; 
+    }
+}
